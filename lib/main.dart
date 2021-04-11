481685cfd0167ded40c16570f8a4b12c2e5f2692
Akash bisariya <akash.bisariya@nagarro.com>
2021-04-11 17:42:24 +0530

    Added Stateful widget example

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,59 @@ import 'package:flutter/widgets.dart';
 import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Flutter App',
-      home: FlutterHome(),
-    )
-
-  );
+  runApp(MaterialApp(
+    title: 'Flutter App',
+    home: StatefulCounter(),
+  ));
 }
 
-class FlutterHome extends StatelessWidget{
+//Stateless widget
+class FlutterHome extends StatelessWidget {
   @override
-  Widget build(BuildContext context ) {
+  Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter App Title")
+    return Scaffold(
+      appBar: AppBar(title: const Text("Flutter App Title")),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        Text(
+          wordPair.asPascalCase,
+          textAlign: TextAlign.start,
         ),
-        body:Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children:[
-            Text(wordPair.asPascalCase,textAlign: TextAlign.start,),
-            Text(WordPair.random().asPascalCase,textAlign: TextAlign.start,)
-          ]
-        ),
+        Text(
+          WordPair.random().asPascalCase,
+          textAlign: TextAlign.start,
+        )
+      ]),
     );
   }
 }
 
+  //Stateful widget
+  class StatefulCounter extends StatefulWidget {
+    @override
+    _StatefulCounterState createState() => _StatefulCounterState();
+  }
 
+  class _StatefulCounterState extends State<StatefulCounter> {
+    int _counter = 0;
+
+    void _increment() {
+      setState(() {
+        _counter++;
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(title: const Text("Flutter Stateful")),
+          body: Container(
+              child: Center(
+                  child: Column(
+            children: [
+              ElevatedButton(onPressed: _increment, child: Text("Increment")),
+              Text('Count $_counter')
+            ],
+          ))));
+    }
+  }
